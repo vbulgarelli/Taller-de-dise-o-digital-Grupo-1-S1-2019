4f4f08599253fa8f01cede2d9c2713caaf8228f7
DRoMarin <47992796+DRoMarin@users.noreply.github.com>
2019-05-07 16:50:35 -0600

    test ready, perdoooon

--- FSM.sv
+++ FSM.sv
@@ -3,55 +3,23 @@
 
 module FSM(
     input logic clk,
-    input manclk,
-    input mode,
     input logic reset,
     input logic opSel,
-    input logic selnum,
-    input logic [7:0] datain,
-    input logic [1:0] seldata,
-    input logic save,
-    input logic datawork,
+    input [7:0] expA, expB,
+    input [22:0] a, b,
+    input signA, signB,
     output reg [31:0] val,
     output reg [3:0] leds
     );
     wire clkdiv;
     reg clkout;
-    reg [31:0] n0, n1;
-    reg [22:0] a, b;
-    reg [7:0] expA, expB;
-    reg signA, signB;
     reg [22:0] shiftManA, shiftManB;
     reg [7:0] shiftmax = 8'd0;
     reg [23:0] sumres;
     reg [1:0] cont = 2'b00;
    
-    Clock_divider(clk,clkdiv);
-    
-    always@*
-    begin
-    if(mode == 0)
-    clkout <= clkdiv;
-    else
-    clkout <= manclk;
-    end
-    
-    always@(posedge save)
-    begin
-    if(datawork==1 && selnum==0)
-    begin
-        n0<=n0 + datain*(2**(8*seldata));
-    end
-    else if (datawork==1&&selnum==1)
-    begin
-        n1<=n1 + datain*(2**(8*seldata));
-    end  
-    
-    {signB,expB,b} <= n1;
-    {signA,expA,a} <= n0;  
-    end
-    
-    always @(posedge clkout && datawork==0)
+
+    always @(posedge clk && reset ==0)
         begin
             case (cont)
                
