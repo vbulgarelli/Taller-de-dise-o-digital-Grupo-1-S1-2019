d01f28c17e8f118a3dba1613a198129e75a7152a
vbulgarelli <silverbax123@gmail.com>
2019-05-21 19:00:08 -0600

    Cuadros y número al azar listos

--- Squareanimation.sv
+++ Squareanimation.sv
@@ -23,6 +23,7 @@
 module Squareanimation(
     input wire CLK,             // board clock: 100 MHz on Arty/Basys3/Nexys
     input wire RST_BTN,         // reset button
+    input boton,                // boton "clock"
     output wire VGA_HS_O,       // horizontal sync output
     output wire VGA_VS_O,       // vertical sync output
     output wire [3:0] VGA_R,    // 4-bit VGA red output
@@ -55,35 +56,51 @@ wire rst = ~RST_BTN;    // reset is active low on Arty & Nexys Video
         .o_x(x), 
         .o_y(y)
     );
-
+    
+    reg [1:0]cont; //contador que vamos a usar para determinar cuando se pinta un cuadro, cambia al pulsar un boton
+    
+    
     // asigna las coordenadas de los cuadrados
     wire sq_a, sq_b, sq_c, sq_d;
     assign sq_a = ((x > 0) & (y >  0) & (x < 320) & (y < 240)) ? 1 : 0;
-    assign sq_b = ((x > 321) & (y > 0) & (x < 640) & (y < 240)) ? 1 : 0;
-    assign sq_c = ((x > 0) & (y > 241) & (x < 320) & (y < 480)) ? 1 : 0;
-    assign sq_d = ((x > 321) & (y > 241) & (x < 640) & (y < 480)) ? 1 : 0;
+    assign sq_b = ((x > 321) & (y > 0) & (x < 640) & (y < 240))& (cont==1) ? 1 : 0;
+    assign sq_c = ((x > 0) & (y > 241) & (x < 320) & (y < 480)) & (cont==2)  ? 1 : 0;
+    assign sq_d = ((x > 321) & (y > 241) & (x < 640) & (y < 480)) & (cont==3) ? 1 : 0;
+
+    reg a,b,c,d;
 
 //    assign VGA_R[3] = sq_b;         // square b is red
 //    assign VGA_G[3] = sq_a | sq_d;  // squares a and d are green
 //    assign VGA_B[3] = sq_c;         // square c is blue
     
-    reg [1:0]cont; //contador que vamos a usar para determinar cuando se pinta un cuadro, cambia al pulsar un boton
-  
+    
+    reg [11:0] random;
+    
+    always@(posedge boton)
+    begin
+        cont <= cont + 1;
+    end
+    // falta crear otro divisor de clock para el pintado automático
+    
   always @* //por ahora pone colores fijos como prueba
   begin  
     case(cont)
-         0: red =4'b1000; 
-         0: green =4'b1000; 
-         0: blue =4'b0010; 
-         1: red =4'b0010; 
-         1: green =4'b1000;
-         1: blue =4'b0000;
-         2: red =4'b0000; 
-         2: green =4'b0010; 
-         2: blue =4'b0100; 
-         3: red =4'b0000; 
-         3: green =4'b0000; 
-         3: blue =4'b1000; 
+         0: red =random[11:8]; 
+         0: green =random[7:4]; 
+         0: blue =random[3:0]; 
+         0: random = random + 3;
+         1: red =random[11:8]; 
+         1: green =random[7:4]; 
+         1: blue =random[3:0]; 
+         1: random = random +1;
+         2: red =random[11:8]; 
+         2: green =random[7:4]; 
+         2: blue =random[3:0]; 
+         2: random = random + 7;
+         3: red =random[11:8]; 
+         3: green =random[7:4]; 
+         3: blue =random[3:0]; 
+         3: random = random + 2;
          
     endcase
    end  
@@ -94,8 +111,9 @@ wire rst = ~RST_BTN;    // reset is active low on Arty & Nexys Video
    assign VGA_B = blue;
    
    // se encienden los cables en las coordenadas del cuadro a
-   assign VGA_R = sq_a;
-   assign VGA_G = sq_a;
-   assign VGA_B = sq_a;
-    
+   assign VGA_R = sq_a | sq_b;
+   assign VGA_G = sq_a | sq_c;
+   assign VGA_B = sq_b | sq_c;
+   
+       
 endmodule
